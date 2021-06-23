7a5750fc693a1586bfe3f075587b2cfe0ba167bb
feliperromao <feliperomaocad@gmail.com>
2021-06-22 23:40:59 -0300

    feat: implement Home screen and New Room screen with navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import {Button} from './components/Button'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom';
+
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 

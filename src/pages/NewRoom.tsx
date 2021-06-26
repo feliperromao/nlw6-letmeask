ddae57421a20cfd782a3f87a31a64f637fa8d94a
feliperromao <feliperomaocad@gmail.com>
2021-06-25 23:57:43 -0300

    feat: implement react new room in firebase

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,5 +1,7 @@
-import { Link } from 'react-router-dom'
+import { FormEvent, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { Button } from '../components/Button'
+import { database } from '../services/firebase'
 import { useAuth } from '../hooks/useAuth'
 
 import illustrationImg from '../assets/images/illustration.svg'
@@ -8,6 +10,24 @@ import '../styles/auth.scss'
 
 export function NewRoom() {
   const {user} = useAuth();
+  const history = useHistory();
+  const [newRoom, setNewRoom] = useState('');
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+    if (newRoom.trim() === '') {
+      console.warn('Nome da sala não pode está vazio')
+      return;
+    }
+
+    const roomRef = database.ref('rooms')
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    })
+
+    history.push(`/rooms/${firebaseRoom.key}`)
+  }
 
   return (
     <div id="page-auth">
@@ -19,12 +39,13 @@ export function NewRoom() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <h1>{user?.name}</h1>
           <h2>Criar uma nova sala</h2>
-          <form>
+          <form onSubmit={handleCreateRoom}>
             <input
               type="text"
-              placeholder="Nome da sala"  
+              placeholder="Nome da sala"
+              onChange={event => setNewRoom(event.target.value)}
+              value={newRoom}
             />
             <Button type="submit">
               Criar sala 
